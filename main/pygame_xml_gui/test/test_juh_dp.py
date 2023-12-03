d4ae82d116bd4315bd05911387920576f19f452c
marcelm9 <marcel.menzel09@gmail.com>
2023-12-03 18:58:17 +0100

    removed pyLayout and modified tests

--- main/pygame_xml_gui/test/test_juh_dp.py
+++ main/pygame_xml_gui/test/test_juh_dp.py
@@ -1,10 +1,10 @@
-import sys, os, pygame
+import os, pygame
 
 from ..src.UserInterface import UserInterface
 
-from PygameXtras import PerformanceGraph
-
 RUN_FOR = -1 # seconds
+
+PATH = os.path.join(os.path.dirname(__file__), "juh_image.png")
 
 class Shift:
     def __init__(self, type_, car, crew, start, end):
@@ -16,22 +16,18 @@ class Shift:
 
 class Program():
     def __init__(self):
-        self.win_w, self.win_h = 800, 600
+        self.win_w, self.win_h = 300, 270
         self.center = (int(self.win_w/2),int(self.win_h/2))
         
         pygame.init()
         self.screen = pygame.display.set_mode((self.win_w, self.win_h))
-        self.fpsclock = pygame.time.Clock()
-        self.fps = 60
 
     def main(self):
-
-        fpslist = []
 
         with open(os.path.join(os.path.dirname(__file__), "test_dp.xml"), "r") as f:
             structure = f.read()
 
-        shift = Shift("JOK", "A-39", ["Marcel Menzel", "Lena Hofmann"], "15.11.2023, 10:00", "15.11.2023, 18:30")
+        shift = Shift("JOK", "A-39", ["Marcel Menzel", "Lena Hofmann", "Anna Kristoferitsch", "Pascal Heinrich"], "15.11.2023, 10:00", "15.11.2023, 18:30")
         
         self.ui = UserInterface()
         self.ui.set_structure(structure)
@@ -42,33 +38,13 @@ class Program():
             "start": shift.start,
             "end": shift.end
         })
-        self.ui.set_pos((150, 50))
+        self.ui.set_pos((0, 0))
         
-        run = True
-        while run:
-            event_list = pygame.event.get()
-            for event in event_list:
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        run = False
-            
-            self.screen.fill((0,0,0))
-            self.ui.draw(self.screen)
-            fps = self.fpsclock.get_fps()
-            fpslist.append(fps)
-            pygame.display.set_caption("Seconds left: " + str(round(RUN_FOR - (len(fpslist)/self.fps), 2)))
-            if len(fpslist) == RUN_FOR * self.fps:
-                run = False
-            
-            
-            pygame.display.flip()
-            self.fpsclock.tick(self.fps)
-
-        pygame.display.set_caption("PerformanceGraph")
-        PerformanceGraph(60, fpslist)
+        self.screen.fill((0,0,0))
+        self.ui.draw(self.screen)
+        pygame.display.flip()
+        pygame.image.save(self.screen, PATH)
+        print(f"Saved to file '{PATH}'")
         
         
 
